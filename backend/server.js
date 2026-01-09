95c78bf76b32373cfdb8e89d2ddff4b967000ded
JPDEV <pamplonajp.45@gmail.com>
2026-01-09 21:42:31 +0800

    putting backend

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const axios = require("axios");
+require("dotenv").config;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
