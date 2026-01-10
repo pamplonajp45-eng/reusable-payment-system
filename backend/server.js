8f1fc78aac9154cfabf17998ab9727a654683b9c
JPDEV <pamplonajp.45@gmail.com>
2026-01-10 19:35:54 +0800

    adding some fetching in localhost

--- backend/server.js
+++ backend/server.js
@@ -8,3 +8,13 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+
+fetch('http://localhost:5000/api/payment/gcash', {
+  body: JSON.stringify({amount:500, email: 'user@gmail.com'})
+}) 
+
+
+ return(
+
+ )
