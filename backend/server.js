2538b572988dabf09257f34aed8299fe46a6e224
JPDEV <pamplonajp.45@gmail.com>
2026-01-11 12:59:17 +0800

    mongoDB connection

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+// need muna mag import
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
@@ -12,3 +13,12 @@ app.use(express.json());
 fetch("http://localhost:5000/api/payment/gcash", {
   body: JSON.stringify({ amount: 500, email: "user@gmail.com" }),
 });
+
+//icoconnect yung monggodb
+mongoose
+  .connect(
+    process.env.MONGODB_URI || "mongodb: //localhost:27107/payment-gcash",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.error("MongoDB Connection Error", err));
